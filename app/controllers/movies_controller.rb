80083ae2d4027fad604b0d057322d3e1b913dc8b
laurenmlang <laurenmlang@gmail.com>
2015-08-05 12:28:59 -0500

    Debugged!

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,8 +4,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @id = params["id"]
-    @movie = Movie.find(@id)
+    @movie = Movie.find(params[:id])
   end
 
   def new_form
@@ -21,27 +20,28 @@ class MoviesController < ApplicationController
     @movie.director_id = params[:director_id]
     @movie.save
 
-    redirect_to("/movies/<%=@movie.id%>")
+    render("show")
   end
 
   def edit_form
-    @movie = Movie.find(params["id"])
+    @movie = Movie.find(params[:id])
   end
 
   def update_row
-    @movie = Movie.find(params["id"])
-    @movie.title = params["title"]
-    @movie.year = params["year"]
-    @movie.duration = params["duration"]
-    @movie.description = params["description"]
-    @movie.image_url = params["image_url"]
-    @movie.director_id = params["director_id"]
+    @movie = Movie.find(params[:id])
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
+    @movie.director_id = params[:director_id]
+    @movie.save
 
     render("show")
   end
 
   def destroy
-    @movie = Movie.find(params["id"])
+    @movie = Movie.find(params[:id])
     @movie.destroy
   end
 end
